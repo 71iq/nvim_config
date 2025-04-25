9d117137babbe952cb22df5e5528c9bd7fcf56e4
Ehab Maali <ehab1.maali@gmail.com>
2025-04-25 14:05:41 +0300

    added nonels instead of nullls, still not working

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -45,7 +45,7 @@ local plugins = {
     end
   },
   {
-    "jose-elias-alvarez/null-ls.nvim",
+    "nvimtools/none-ls.nvim",
     event = "VeryLazy",
     opts = function()
       return require "custom.configs.null-ls"
