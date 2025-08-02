f35783fa6eb6623b00d65eabe9458b80f07d6ad9
Ehab Maali <ehab1.maali@gmail.com>
2025-08-02 10:07:56 +0300

    added shortcut to remove long comments in c and comments in py

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -32,3 +32,43 @@ map("n", "<leader>ri", function()
   }
   vim.api.nvim_buf_set_lines(0, 0, 0, false, lines)
 end, { desc = "Replace #includes with standard header block" })
+
+vim.keymap.set("n", "<leader>rlc", function()
+  local start_line = nil
+  local end_line = nil
+  local lines_to_delete = {}
+
+  for i = 1, vim.fn.line("$") do
+    local line = vim.fn.getline(i)
+
+    if not start_line and line:match("^%s*/%*%*") then
+      start_line = i
+    end
+
+    if start_line and line:match("%*/%s*$") then
+      end_line = i
+      table.insert(lines_to_delete, { start_line, end_line })
+      start_line = nil
+      end_line = nil
+    end
+  end
+
+  for i = #lines_to_delete, 1, -1 do
+    local range = lines_to_delete[i]
+    vim.api.nvim_buf_set_lines(0, range[1] - 1, range[2], false, {})
+  end
+end, { desc = "Remove /** */ Doc Comments" })
+
+vim.keymap.set("n", "<leader>rcp", function()
+  local total_lines = vim.fn.line("$")
+
+  for i = 1, total_lines do
+    local line = vim.fn.getline(i)
+    if line:match("^%s*#") then
+      vim.fn.setline(i, "")
+    elseif line:match("#") then
+      local uncommented = line:gsub("#.*", "")
+      vim.fn.setline(i, uncommented)
+    end
+  end
+end, { desc = "Remove Python Comments" })
